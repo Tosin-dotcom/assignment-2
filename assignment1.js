b469b35806eba822d0b6816c6276ef58019f6ffe
Tosin <tosiniyiola00@gmail.com>
2022-07-28 11:18:39 +0100

    Initial commit

---
+++ assignment1.js
@@ -0,0 +1,15 @@
+
+for (let i = 1; i <= 200; i++) {
+    if (i % 6 == 0 && i % 8 == 0) {
+        console.log("ShamsTIIDELAB")
+    }
+    else if (i % 6 == 0) {
+        console.log("Shams")
+    } 
+    else if (i % 8 == 0) {
+        console.log("TIIDELAB")
+    }
+    else {
+        console.log(i)
+    }
+}
